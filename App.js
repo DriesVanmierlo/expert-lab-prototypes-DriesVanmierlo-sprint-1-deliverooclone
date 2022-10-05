277cdc74b7fbc65c5240b6def33b0fe9316638af
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-10-05 12:06:29 +0200

    Start of delivery screen, made white box with time info

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import { store } from "./store";
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ export default function App() {
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
           <Stack.Screen name="Basket" component={BasketScreen} options={{presentation: 'modal', headerShown: false}} />
           <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen} options={{ presentation: 'fullScreenModal', headerShown: false}} />
+          <Stack.Screen name="Delivery" component={DeliveryScreen} options={{ presentation: 'fullScreenModal', headerShown: false}} />
         </Stack.Navigator>
       </TailwindProvider>
       </Provider>
