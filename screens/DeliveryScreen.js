f6a09f0cfdaf4d5e1edc6425f87476f2dade8e4c
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-10-05 13:31:40 +0200

    Finished delivery screen

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -6,6 +6,7 @@ import { selectRestaurant } from '../features/restaurantSlice'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { XMarkIcon } from 'react-native-heroicons/solid'
 import * as Progress from 'react-native-progress'
+import MapView, { Marker } from 'react-native-maps'
 
 const DeliveryScreen = () => {
     const navigation = useNavigation();
@@ -37,6 +38,43 @@ const DeliveryScreen = () => {
             <Text className='mt-3 text-gray-500'>Your order at {restaurant.title} is being prepared</Text>
         </View>
       </SafeAreaView>
+
+        <MapView 
+            initialRegion={{
+                latitude: restaurant.lat,
+                longitude: restaurant.long,
+                latitudeDelta: 0.005,
+                longitudeDelta:0.005
+            }}
+            className='flex-1 -mt-10 z-0'
+            mapType="mutedStandard"
+        >
+            <Marker 
+                coordinate={{
+                    latitude: restaurant.lat,
+                    longitude: restaurant.long
+                }}
+                title={restaurant.title}
+                description={restaurant.short_description}
+                identifier="origin"
+                pinColor="#00CCBB"
+            />
+        </MapView>
+
+        <SafeAreaView className='bg-white flex-row items-center space-x-5 h-28'>
+            <Image 
+                source={{
+                    uri: "https://links.papareact.com/wru"
+                }}
+                className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+            />
+            <View className='flex-1'>
+                <Text className='text-lg'>Dries Vanmierlo</Text>
+                <Text className='text-gray-400'>Your Rider</Text>
+            </View>
+            <Text className='text-[#00CCBB] text-lg mr-5 font-bold'>Call</Text>
+        </SafeAreaView>
+
     </View>
   )
 }
